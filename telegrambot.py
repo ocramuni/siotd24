7df55183ea07c5115e83f3808cb5d5d3b6ce53fe
Marco Giunta <giunta.marco@spes.uniud.it>
2025-02-14 21:31:57 +0100

    set_command does not work on application init. start bot command is a best practice. Add more command to manage temperature.

--- telegrambot.py
+++ telegrambot.py
@@ -8,28 +8,32 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env
 TELEGRAM_TOKEN=os.getenv("TELEGRAM_TOKEN")
 
+async def set_command(application: Application) -> None:
+    await application.bot.set_my_commands([
+        ('picture', 'Get a picture of the room'),
+        ('people', 'Get the number of the people in the room'),
+        ('temperature', 'Get room temperature'),
+        ('offset', 'Get/Set temperature offset'),
+        ('minimum', 'Get/Set the minimum temperature'),
+        ('maximum', 'Get/Set the maximum temperature'),
+    ])
+
 class TelegramBot:
-    def __init__(self, my_camera, my_led, my_sensor, offset):
-        self.application = Application.builder().token(TELEGRAM_TOKEN).post_init(self.post_init).build()
+    def __init__(self, my_camera, my_led, my_sensor, temperature):
+        self.application = Application.builder().token(TELEGRAM_TOKEN).build()
         self.my_camera = my_camera
         self.my_led = my_led
         self.my_sensor = my_sensor
-        self.offset = offset # passed by reference
+        self.temperature = temperature # temperature dictionary passed by reference
 
-        start_handler = CommandHandler('start', self.start)
+        start_handler = CommandHandler(['start','help'], self.get_start)
         self.application.add_handler(start_handler)
-
-        led_on_handler = CommandHandler('on', self.led_on)
-        self.application.add_handler(led_on_handler)
-
-        led_off_handler = CommandHandler('off', self.led_off)
-        self.application.add_handler(led_off_handler)
 
         get_picture_handler = CommandHandler('picture', self.get_picture)
         self.application.add_handler(get_picture_handler)
 
-        led_status_handler = CommandHandler('status', self.led_status)
-        self.application.add_handler(led_status_handler)
+        get_people_handler = CommandHandler('people', self.get_people)
+        self.application.add_handler(get_people_handler)
 
         get_temperature_handler = CommandHandler('temperature', self.get_temperature)
         self.application.add_handler(get_temperature_handler)
@@ -37,24 +41,16 @@ class TelegramBot:
         set_offset_handler = CommandHandler('offset', self.set_offset)
         self.application.add_handler(set_offset_handler)
 
-    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text="I'm a bot, please talk to me!")
+        set_maximum_handler = CommandHandler('maximum', self.set_maximum)
+        self.application.add_handler(set_maximum_handler)
 
-    async def led_on(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        text = self.my_led.on()
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=text)
+        set_minimum_handler = CommandHandler('minimum', self.set_minimum)
+        self.application.add_handler(set_minimum_handler)
 
-    async def led_off(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        text = self.my_led.off()
+    async def get_start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        room = context.bot_data['room']
         await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=text)
-
-    async def led_status(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        text = self.my_led.status()
-        await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=text)
+                                       text=f"I'm monitoring temperature in room {room}")
 
     def _to_bytes(self, image):
         is_success, im_buf_arr = cv2.imencode(".jpg", image)
@@ -69,11 +65,16 @@ class TelegramBot:
                                      photo=image_bytes,
                                      caption=f"Room {room}")
 
+    async def get_people(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        annotated_image, detections = self.my_camera.get_picture()
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=detections)
+
     async def get_temperature(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
         room = context.bot_data['room']
         my_temperature = self.my_sensor.read('temperature')
         if my_temperature is not None:
-            my_temperature += self.offset[0]
+            my_temperature += self.temperature['offset']
             text = 'Temperature in room {0} is: {1:0.1f}C'.format(room, my_temperature)
         else:
             text = 'No sensor found'
@@ -81,19 +82,22 @@ class TelegramBot:
                                        text=text)
 
     async def set_offset(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-        #print(update.effective_chat.id)
         result = [arg for arg in context.args if arg.lstrip('-+').isdigit()]  # may be negative
         if len(result) != 0:
-            self.offset[0] = int(result[0])
+            self.temperature['offset'] = int(result[0])
         await context.bot.send_message(chat_id=update.effective_chat.id,
-                                       text=f"Current offset is: {self.offset[0]}.")
+                                       text=f"Current offset is: {self.temperature['offset']}.")
 
-    async def post_init(self, application: Application) -> None:
-        await application.bot.set_my_commands([
-            ('picture', 'Get a picture of the room'),
-            ('temperature', 'Get room temperature'),
-            ('offset', 'Get/Set offset'),
-            ('on', 'Turn LED on'),
-            ('off', 'Turn LED off'),
-            ('status', 'Get LED status'),
-        ])
+    async def set_minimum(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        result = [arg for arg in context.args if arg.lstrip('-+').isdigit()]  # may be negative
+        if len(result) != 0:
+            self.temperature['min'] = int(result[0])
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=f"Current minimum temperature is: {self.temperature['min']}.")
+
+    async def set_maximum(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        result = [arg for arg in context.args if arg.lstrip('-+').isdigit()]  # may be negative
+        if len(result) != 0:
+            self.temperature['max'] = int(result[0])
+        await context.bot.send_message(chat_id=update.effective_chat.id,
+                                       text=f"Current maximum temperature is: {self.temperature['max']}.")
